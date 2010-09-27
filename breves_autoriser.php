5afcbc484f35b8b379626ec13c99ed65af7db0ce
Matthieu Marcillaud <marcimat@rezo.net>
2010-09-27 19:51:01 +0000

    Ajout des brèves sur les rubriques et accueil
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/breves@41285 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- breves_autoriser.php
+++ breves_autoriser.php
@@ -18,7 +18,35 @@ function breves_autoriser(){}
 function autoriser_breves_bouton_dist($faire, $type='', $id=0, $qui = NULL, $opt = NULL){
 	return 	($GLOBALS['meta']["activer_breves"] != "non");
 }
+function autoriser_breve_creer_bouton_dist($faire, $type, $id, $qui, $opt){
+	return 	($GLOBALS['meta']["activer_breves"] != "non");
+}
 
+
+
+// Autoriser a creer une breve dans la rubrique $id
+// http://doc.spip.org/@autoriser_rubrique_creerbrevedans_dist
+function autoriser_rubrique_creerbrevedans_dist($faire, $type, $id, $qui, $opt) {
+	$r = sql_fetsel("id_parent", "spip_rubriques", "id_rubrique=".sql_quote($id));
+	return
+		$id
+		AND ($r['id_parent']==0)
+		AND ($GLOBALS['meta']["activer_breves"]!="non")
+		AND autoriser('voir','rubrique',$id);
+}
+
+
+// Autoriser a modifier la breve $id
+// = admins & redac si la breve n'est pas publiee
+// = admins de rubrique parente si publiee
+// http://doc.spip.org/@autoriser_breve_modifier_dist
+function autoriser_breve_modifier_dist($faire, $type, $id, $qui, $opt) {
+	$r = sql_fetsel("id_rubrique,statut", "spip_breves", "id_breve=".sql_quote($id));
+	return
+		($r['statut'] == 'publie')
+			? autoriser('publierdans', 'rubrique', $r['id_rubrique'], $qui, $opt)
+			: in_array($qui['statut'], array('0minirezo', '1comite'));
+}
 
 
 ?>
