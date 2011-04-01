023bbf37fff5cff0da0942aab4aee4a3d1e3b553
Cerdic <cedric@yterium.com>
2011-04-01 08:27:57 +0000

    Utiliser le flag exist=true en retour de trouver_table pour etre sur que la table existe et declencher l'upgrade plutot que la creation.
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/breves@46233 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- base/breves_upgrade.php
+++ base/breves_upgrade.php
@@ -17,6 +17,19 @@
  * @param string $version_cible
  */
 function breves_upgrade($nom_meta_base_version,$version_cible){
+	// cas particulier :
+	// si plugin pas installe mais que la table existe
+	// considerer que c'est un upgrade depuis v 1.0.0
+	// pour gerer l'historique des installations SPIP <=2.1
+	if (!isset($GLOBALS['meta'][$nom_meta_base_version])){
+		$trouver_table = charger_fonction('trouver_table','base');
+		if ($desc = $trouver_table('spip_breves')
+		  AND isset($desc['exist'])){
+			ecrire_meta($nom_meta_base_version,'1.0.0');
+		}
+		// si pas de table en base, on fera une simple creation de base
+	}
+	
 	$maj = array();
 	$maj['create'] = array(
 		array('maj_tables',array('spip_breves')),
