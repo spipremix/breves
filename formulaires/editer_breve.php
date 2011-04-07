7cf00a4be6380d541d4ca7792d72affaf3e74496
Cerdic <cedric@yterium.com>
2011-04-07 16:28:45 +0000

    traitement id_parent/id_rubrique sur breves et site, suite a http://core.spip.org/projects/spip/repository/revisions/17674

--- formulaires/editer_breve.php
+++ formulaires/editer_breve.php
@@ -19,10 +19,8 @@ function formulaires_editer_breve_charger_dist($id_breve='new', $id_rubrique=0, 
 	$valeurs = formulaires_editer_objet_charger('breve',$id_breve,$id_rubrique,$lier_trad,$retour,$config_fonc,$row,$hidden);
 	// un bug a permis a un moment que des breves soient dans des sous rubriques
 	// lorsque ce cas se presente, il faut relocaliser la breve dans son secteur, plutot que n'importe ou
-	if ($valeurs['id_rubrique'])
-		$valeurs['id_parent'] = sql_getfetsel('id_secteur','spip_rubriques','id_rubrique='.intval($valeurs['id_rubrique']));
-	// et on enleve id_rubrique des valeurs saisies (c'est id_parent)
-	unset($valeurs['id_rubrique']);
+	if ($valeurs['id_parent'])
+		$valeurs['id_parent'] = sql_getfetsel('id_secteur','spip_rubriques','id_rubrique='.intval($valeurs['id_parent']));
 	return $valeurs;
 }
 
