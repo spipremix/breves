b9b00e7e1ddec9754135ab65c33553a2ee62c700
jrynik <jrynik@yahoo.co.uk>
2011-06-02 06:58:47 +0000

    langues (breves)
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/breves@48370 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/breves_sk.php
+++ lang/breves_sk.php
@@ -21,13 +21,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_modifier_breve' => 'Upraviť túto správu',
 	'icone_nouvelle_breve' => 'Napísať novú správu',
 	'info_1_breve' => '1 správa',
-	'info_aucun_breve' => 'Aucune brève', # NEW
+	'info_aucun_breve' => 'Žiadne novinky',
 	'info_breves' => 'Does your site use the news system?',
 	'info_breves_02' => 'Novinky',
 	'info_breves_valider' => 'Novinky na schválenie',
 	'info_gauche_numero_breve' => 'NEWS ITEM NUMBER',
-	'info_nb_breves' => '@nb@ brèves', # NEW
-	'item_breve_proposee' => 'Předložené novinky', # NEW
+	'info_nb_breves' => '@nb@ noviniek',
+	'item_breve_proposee' => 'Novinka bola odoslaná',
 	'item_breve_refusee' => 'Odmietnutá novinka',
 	'item_breve_validee' => 'Schválená novinka',
 	'item_non_utiliser_breves' => 'Do not use the news',
@@ -40,7 +40,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'texte_breves' => 'News are short and simple texts which allow
 	the publication online of concise information, the management of
 	a press review, a calendar of events...',
-	'titre_breve_proposee' => 'Předložená novinka', # NEW
+	'titre_breve_proposee' => 'Novinka bola odoslaná',
 	'titre_breve_publiee' => 'News item published',
 	'titre_breve_refusee' => 'News item rejected',
 	'titre_breves' => 'Novinky',
