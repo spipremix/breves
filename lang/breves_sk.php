2837f1754447367df94bc40aa6e5bdca763f86fb
salvatore <salvatore@rezo.net>
2011-08-05 14:18:37 +0000

    langues (breves)

--- lang/breves_sk.php
+++ lang/breves_sk.php
@@ -44,7 +44,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_breve_refusee' => 'Novinka zamietnutá',
 	'titre_breves' => 'Novinky',
 	'titre_langue_breve' => 'JAZYK NOVINKY',
-	'titre_nouvelle_breve' => 'Nouvelle brève', # NEW
+	'titre_nouvelle_breve' => 'Nová novinka',
 	'titre_page_breves' => 'Novinky'
 );
 
