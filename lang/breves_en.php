6f065d82a76ae3187e8961f1b87150634fb418d1
salvatore <salvatore@rezo.net>
2011-12-12 21:13:26 +0000

    langues (breves)
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/breves@55453 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/breves_en.php
+++ lang/breves_en.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://www.spip.net/trad-lang/
+// extrait automatiquement de http://trad.spip.org
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -12,7 +12,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'breves' => 'News items',
 
 	// E
-	'entree_breve_publiee' => 'Is this news item ready to be published?', # MODIF
+	'entree_breve_publiee' => 'This news item is:',
 	'entree_texte_breve' => 'Text of news item',
 
 	// I
@@ -25,26 +25,26 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_breves' => 'Does your site use news items?',
 	'info_breves_02' => 'News',
 	'info_breves_valider' => 'News items awaiting validation',
-	'info_gauche_numero_breve' => 'NEWS ITEM NUMBER', # MODIF
+	'info_gauche_numero_breve' => 'News item number',
 	'info_nb_breves' => '@nb@ news items',
-	'item_breve_proposee' => 'News item submitted', # MODIF
-	'item_breve_refusee' => 'NO - News item rejected', # MODIF
-	'item_breve_validee' => 'YES - News item validated', # MODIF
+	'item_breve_proposee' => 'submitted',
+	'item_breve_refusee' => 'rejected',
+	'item_breve_validee' => 'validated',
 	'item_non_utiliser_breves' => 'Do not use news items',
 	'item_utiliser_breves' => 'Use news items',
 
 	// L
-	'logo_breve' => 'NEWS ITEM LOGO', # MODIF
+	'logo_breve' => 'News item logo',
 
 	// T
 	'texte_breves' => 'News items are brief messages which allow the publication of
  news flashes, managing of press reviews, an events calendar etc.',
-	'titre_breve_proposee' => 'News item submitted',
+	'titre_breve_proposee' => 'News proposed',
 	'titre_breve_publiee' => 'News item published',
 	'titre_breve_refusee' => 'News item rejected',
 	'titre_breves' => 'News',
-	'titre_langue_breve' => 'NEWS ITEM LANGUAGE', # MODIF
-	'titre_nouvelle_breve' => 'Nouvelle brève', # NEW
+	'titre_langue_breve' => 'News item language',
+	'titre_nouvelle_breve' => 'New news item',
 	'titre_page_breves' => 'News'
 );
 
