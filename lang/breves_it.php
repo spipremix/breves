d9e93f43c78d4b68a18a0a77b014b44cc7744799
elvis.deltedesco <elvis.deltedesco@phante.com>
2012-03-03 10:43:11 +0000

    langues (breves)

--- lang/breves_it.php
+++ lang/breves_it.php
@@ -12,7 +12,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'breves' => 'Brevi',
 
 	// E
-	'entree_breve_publiee' => 'Questa breve deve essere pubblicata?', # MODIF
+	'entree_breve_publiee' => 'Questa breve è : ',
 	'entree_texte_breve' => 'Testo della breve',
 
 	// I
@@ -25,16 +25,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_breves' => 'Il tuo sito dovrà utilizzare il sistema di brevi?',
 	'info_breves_02' => 'Brevi',
 	'info_breves_valider' => 'Brevi da convalidare',
-	'info_gauche_numero_breve' => 'BREVE NUMERO', # MODIF
+	'info_gauche_numero_breve' => 'Breve numero',
 	'info_nb_breves' => '@nb@ brevi',
-	'item_breve_proposee' => 'Breve proposta', # MODIF
-	'item_breve_refusee' => 'NO - Breve rifiutata', # MODIF
-	'item_breve_validee' => 'SI - Breve convalidata', # MODIF
+	'item_breve_proposee' => 'proposta',
+	'item_breve_refusee' => 'rifiutata',
+	'item_breve_validee' => 'validatata',
 	'item_non_utiliser_breves' => 'Non utilizzare le brevi',
 	'item_utiliser_breves' => 'Utilizza le brevi',
 
 	// L
-	'logo_breve' => 'LOGO DELLA BREVE', # MODIF
+	'logo_breve' => 'Logo della breve',
 
 	// T
 	'texte_breves' => 'Le brevi sono dei testi corti e semplici che consentono
@@ -44,8 +44,8 @@ di gestire una rassegna stampa o un calendario di eventi, ecc.',
 	'titre_breve_publiee' => 'Breve pubblicata',
 	'titre_breve_refusee' => 'Breve rifiutata',
 	'titre_breves' => 'Le brevi',
-	'titre_langue_breve' => 'LINGUA DELLA BREVE', # MODIF
-	'titre_nouvelle_breve' => 'Nouvelle brève', # NEW
+	'titre_langue_breve' => 'Lingua della breve',
+	'titre_nouvelle_breve' => 'Nuova breve',
 	'titre_page_breves' => 'Brevi'
 );
 
