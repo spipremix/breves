0a8de38adf640610a07722e23422300cf0c4ecfc
salvatore <salvatore@rezo.net>
2013-07-07 03:37:20 +0000

    langues (breves)
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/breves@74150 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/breves_pt.php
+++ lang/breves_pt.php
@@ -8,43 +8,43 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'breve' => 'Not', # NEW
-	'breves' => 'Not',
+	'breve' => 'Notícia',
+	'breves' => 'Notícia',
 
 	// E
-	'entree_breve_publiee' => 'Colocar a not', # MODIF
-	'entree_texte_breve' => 'Escrever o texto da not',
+	'entree_breve_publiee' => 'Esta notícia é: ',
+	'entree_texte_breve' => 'Escrever o texto da notícia',
 
 	// I
 	'icone_breves' => 'Notícias',
-	'icone_ecrire_nouvel_article' => 'As not',
+	'icone_ecrire_nouvel_article' => 'As notícias contidas nesta rubrica',
 	'icone_modifier_breve' => 'Modificar esta notícia',
 	'icone_nouvelle_breve' => 'Escrever uma notícia curta',
 	'info_1_breve' => '1 notícia',
-	'info_aucun_breve' => 'Sem not', # NEW
+	'info_aucun_breve' => 'Sem notícias',
 	'info_breves' => 'O seu sítio utiliza o sistema de notícias',
-	'info_breves_02' => 'Not',
+	'info_breves_02' => 'Notícia',
 	'info_breves_valider' => 'Notícias a validar',
-	'info_gauche_numero_breve' => 'NOTÍCIA NÚMERO', # MODIF
-	'info_nb_breves' => '@nb@ brèves', # NEW
-	'item_breve_proposee' => 'not', # MODIF
-	'item_breve_refusee' => 'Not', # MODIF
-	'item_breve_validee' => 'Not', # MODIF
+	'info_gauche_numero_breve' => 'NOTÍCIA NÚMERO',
+	'info_nb_breves' => '@nb@ notícias',
+	'item_breve_proposee' => 'notícia proposta',
+	'item_breve_refusee' => 'Notícia recusada',
+	'item_breve_validee' => 'Notícia aceite',
 	'item_non_utiliser_breves' => 'Não utilizar as notícias',
-	'item_utiliser_breves' => 'Utilizar as not',
+	'item_utiliser_breves' => 'Utilizar as notícias',
 
 	// L
-	'logo_breve' => 'Logotipo da not', # MODIF
+	'logo_breve' => 'Logotipo da notícia',
 
 	// T
-	'texte_breves' => 'As not',
+	'texte_breves' => 'As notícias são mensagens curtas que permitem a publicação de informações concisas, a gestão de revisões de imprensa, de um calendário de eventos...',
 	'titre_breve_proposee' => 'Notícia proposta',
-	'titre_breve_publiee' => 'T',
-	'titre_breve_refusee' => 'T',
-	'titre_breves' => 'T',
-	'titre_langue_breve' => 'Idioma da not', # MODIF
-	'titre_nouvelle_breve' => 'Nouvelle brève', # NEW
-	'titre_page_breves' => 'T'
+	'titre_breve_publiee' => 'Notícia publicada',
+	'titre_breve_refusee' => 'Notícia recusada',
+	'titre_breves' => 'Notícias',
+	'titre_langue_breve' => 'Idioma da notícia',
+	'titre_nouvelle_breve' => 'Nova notícia',
+	'titre_page_breves' => 'Notícias'
 );
 
 ?>
