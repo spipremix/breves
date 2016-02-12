19656b3c99e36bbc1bd8f8e01b1c65a0a56fd909
spip <spip@allesfrans.com>
2016-02-12 04:32:53 +0000

    [Salvatore] [source:_core_/plugins/breves/lang/ breves] Export depuis http://trad.spip.net de la langue nl

--- lang/breves_nl.php
+++ lang/breves_nl.php
@@ -23,30 +23,30 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_modifier_breve' => 'Dit bericht wijzigen',
 	'icone_nouvelle_breve' => 'Een nieuwsbericht schrijven',
 	'info_1_breve' => '1 bericht',
-	'info_aucun_breve' => 'Geen nieuws',
-	'info_breves' => 'Werkt je website met het systeem van berichten?',
+	'info_aucun_breve' => 'Geen nieuwsbericht',
+	'info_breves' => 'Werkt je website met nieuwsberichten?',
 	'info_breves_02' => 'nieuwsberichten',
 	'info_breves_valider' => 'Berichten ter goedkeuring',
 	'info_gauche_numero_breve' => 'Bericht nummer',
 	'info_nb_breves' => '@nb@ berichten',
-	'item_breve_proposee' => 'ter evaluatie voorgesteld',
+	'item_breve_proposee' => 'ter evaluatie',
 	'item_breve_refusee' => 'geweigerd',
 	'item_breve_validee' => 'goedgekeurd',
-	'item_non_utiliser_breves' => 'Geen gebruik maken van nieuwsberichten',
+	'item_non_utiliser_breves' => 'Nieuwsberichten niet gebruiken',
 	'item_utiliser_breves' => 'Nieuwsberichten gebruiken',
 
 	// L
 	'logo_breve' => 'Logo van het nieuwsbericht',
 
 	// T
-	'texte_breves' => 'Berichten zijn korte en eenvoudige teksten die je toelaten nieuws snel online te brengen, een persbericht op te stellen, een evenementenkalender te maken, ...',
+	'texte_breves' => 'Nieuwsberichten zijn korte en eenvoudige teksten die snel online geplaatst kunnen worden: een persbericht opstellen, een evenementenkalender maken, ...',
 	'titre_breve_proposee' => 'Voorgesteld bericht',
 	'titre_breve_publiee' => 'Gepubliceerd bericht',
 	'titre_breve_refusee' => 'Geweigerd bericht',
 	'titre_breves' => 'Berichten',
 	'titre_langue_breve' => 'Taal van het nieuwsbericht',
 	'titre_nouvelle_breve' => 'Nieuwe nieuwsbericht',
-	'titre_page_breves' => 'Berichten'
+	'titre_page_breves' => 'Nieuwsberichten'
 );
 
 ?>
