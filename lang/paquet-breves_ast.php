70cedf2712483d5eefe91fff681e2132ff6af416
xuacusk8 <xuacusk8@gmail.com>
2017-11-14 17:43:36 +0000

    [Salvatore] [source:_core_/plugins/breves/lang/ paquet-breves] Export depuis http://trad.spip.net de la langue ast

---
+++ lang/paquet-breves_ast.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-breves?lang_cible=ast
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'breves_description' => 'Les breves son informaciones curties, ensin autor.',
+	'breves_slogan' => 'Alministración de noticies curties en SPIP'
+);
