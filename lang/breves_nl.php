02e5eba235a8148251fb2d883da1f8028d8fe05a
hanjo.spip <hanjo.spip@gmail.com>
2018-01-12 04:36:20 +0000

    [Salvatore] [source:_core_/plugins/breves/lang/ breves] Export depuis http://trad.spip.net de la langue nl

--- lang/breves_nl.php
+++ lang/breves_nl.php
@@ -10,41 +10,41 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'breve' => 'Nieuwsbericht',
-	'breves' => 'Nieuwsberichten',
+	'breve' => 'Nieuwsflits',
+	'breves' => 'Nieuwsflitsen',
 
 	// E
-	'entree_breve_publiee' => 'Dit bericht is:',
-	'entree_texte_breve' => 'Tekst van het bericht',
+	'entree_breve_publiee' => 'Deze nieuwsflits is:',
+	'entree_texte_breve' => 'Tekst van de nieuwsflits',
 
 	// I
-	'icone_breves' => 'Berichten',
-	'icone_ecrire_nouvel_article' => 'De berichten van deze rubriek',
-	'icone_modifier_breve' => 'Dit bericht wijzigen',
-	'icone_nouvelle_breve' => 'Een nieuwsbericht schrijven',
-	'info_1_breve' => '1 bericht',
-	'info_aucun_breve' => 'Geen nieuwsbericht',
-	'info_breves' => 'Werkt je website met nieuwsberichten?',
-	'info_breves_02' => 'nieuwsberichten',
-	'info_breves_valider' => 'Berichten ter goedkeuring',
-	'info_gauche_numero_breve' => 'Bericht nummer',
+	'icone_breves' => 'Nieuwsflitsen',
+	'icone_ecrire_nouvel_article' => 'De nieuwsflitsen van deze rubriek',
+	'icone_modifier_breve' => 'Deze nieuwsflits wijzigen',
+	'icone_nouvelle_breve' => 'Een nieuwsflits schrijven',
+	'info_1_breve' => '1 nieuwsflits',
+	'info_aucun_breve' => 'Geen nieuwsflitsen',
+	'info_breves' => 'Werkt je website met nieuwsflitsen?',
+	'info_breves_02' => 'Nieuwsflitsen',
+	'info_breves_valider' => 'Nieuwsflitsen ter goedkeuring',
+	'info_gauche_numero_breve' => 'Nieuwsflits nummer',
 	'info_nb_breves' => '@nb@ berichten',
 	'item_breve_proposee' => 'ter evaluatie',
 	'item_breve_refusee' => 'geweigerd',
 	'item_breve_validee' => 'goedgekeurd',
-	'item_non_utiliser_breves' => 'Nieuwsberichten niet gebruiken',
-	'item_utiliser_breves' => 'Nieuwsberichten gebruiken',
+	'item_non_utiliser_breves' => 'Nieuwsflitsen niet gebruiken',
+	'item_utiliser_breves' => 'Nieuwsflitsen gebruiken',
 
 	// L
-	'logo_breve' => 'Logo van het nieuwsbericht',
+	'logo_breve' => 'Logo van de nieuwsflits',
 
 	// T
-	'texte_breves' => 'Nieuwsberichten zijn korte en eenvoudige teksten die snel online geplaatst kunnen worden: een persbericht opstellen, een evenementenkalender maken, ...',
-	'titre_breve_proposee' => 'Voorgesteld bericht',
-	'titre_breve_publiee' => 'Gepubliceerd bericht',
-	'titre_breve_refusee' => 'Geweigerd bericht',
-	'titre_breves' => 'Berichten',
-	'titre_langue_breve' => 'Taal van het nieuwsbericht',
-	'titre_nouvelle_breve' => 'Nieuwe nieuwsbericht',
-	'titre_page_breves' => 'Nieuwsberichten'
+	'texte_breves' => 'Nieuwsflitsen zijn korte en eenvoudige teksten die snel online geplaatst kunnen worden: een persbericht, een evenementenkalender, ...',
+	'titre_breve_proposee' => 'Voorgestelde nieuwsflits',
+	'titre_breve_publiee' => 'Gepubliceerde nieuwsflits',
+	'titre_breve_refusee' => 'Geweigerde nieuwsflits',
+	'titre_breves' => 'De nieuwsflitsen',
+	'titre_langue_breve' => 'Taal van de nieuwsflits',
+	'titre_nouvelle_breve' => 'Nieuwe nieuwsflits',
+	'titre_page_breves' => 'Nieuwsflitsen'
 );
