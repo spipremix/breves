53e0aa7f391f15dd3acc674ed14e2a11fae0ab75
JamesRezo <james@rezo.net>
2018-06-07 19:28:42 +0000

    stdlayout+semver breves merge spip-3.1 -> breves 1.3
    git-svn-id: svn://zone.spip.org/spip-zone/_composer_/breves/branches/1.3@110581 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/breves_ca.php
+++ lang/breves_ca.php
@@ -1,9 +1,11 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de https://trad.spip.net/tradlang_module/breves?lang_cible=ca
+// extrait automatiquement de http://trad.spip.net/tradlang_module/breves?lang_cible=ca
 // ** ne pas modifier le fichier **
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
--- lang/paquet-breves_nl.php
+++ lang/paquet-breves_nl.php
@@ -1,14 +1,16 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-breves?lang_cible=nl
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-breves?lang_cible=nl
 // ** ne pas modifier le fichier **
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'breves_description' => 'Nieuwsberiechten zijn kortnieuws zonder auteur.',
+	'breves_description' => 'Nieuwsberichten zijn kort nieuws zonder auteur.',
 	'breves_slogan' => 'Beheer van nieuwsberichten in SPIP'
 );
 
