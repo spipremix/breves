42d3a54076dd64289ec1bd39e7500708988643a0
origenius <origenius@mvb.biglobe.ne.jp>
2018-07-07 03:39:12 +0000

    [Salvatore] [source:_core_/plugins/breves/lang/ breves] Export depuis http://trad.spip.net de la langue ja

--- lang/breves_ja.php
+++ lang/breves_ja.php
@@ -10,37 +10,41 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
+	'breve' => 'ニュース',
 	'breves' => 'ニュース',
 
 	// E
-	'entree_breve_publiee' => 'このニュースは公開されるはずですか？', # MODIF
+	'entree_breve_publiee' => 'このニュースは：',
 	'entree_texte_breve' => 'ニュースの文',
 
 	// I
 	'icone_breves' => 'ニュース',
 	'icone_ecrire_nouvel_article' => 'このセクションのニュース',
-	'icone_modifier_breve' => 'このニュースを修正',
-	'icone_nouvelle_breve' => '新しいニュースを書く',
-	'info_1_breve' => '１ニュース',
-	'info_breves' => 'あなたのサイトで、ニュース・システムをお使いですか？',
+	'icone_modifier_breve' => 'このニュースを修正する',
+	'icone_nouvelle_breve' => '新規ニュースを書く',
+	'info_1_breve' => 'ニュース１件',
+	'info_aucun_breve' => 'どんなニュースもない',
+	'info_breves' => 'ニュース・システムをお使いですか？',
 	'info_breves_02' => 'ニュース',
 	'info_breves_valider' => '検査済みニュース',
-	'info_gauche_numero_breve' => 'ニュース番号', # MODIF
-	'item_breve_proposee' => 'ニュースは提出された', # MODIF
-	'item_breve_refusee' => 'いいえ - ニュースは拒否された', # MODIF
-	'item_breve_validee' => 'はい - 検査済みニュース', # MODIF
+	'info_gauche_numero_breve' => 'ニュース番号',
+	'info_nb_breves' => 'ニュース@nb@件',
+	'item_breve_proposee' => 'ニュースは提出された',
+	'item_breve_refusee' => 'ニュースは拒否された',
+	'item_breve_validee' => '確認済みのニュース',
 	'item_non_utiliser_breves' => 'ニュースを使わない',
 	'item_utiliser_breves' => 'ニュースを使う',
 
 	// L
-	'logo_breve' => 'ニュースのロゴ', # MODIF
+	'logo_breve' => 'ニュースのロゴ',
 
 	// T
 	'texte_breves' => 'ニュースは短くて簡単な文章です。オンラインで簡潔な情報を公表したり、プレスレビューに使ったり、イベントのカレンダーなどに使えます。',
-	'titre_breve_proposee' => '提出されたニュース記事',
-	'titre_breve_publiee' => '公開されたニュース記事',
-	'titre_breve_refusee' => '受け付けられなかったニュース記事',
+	'titre_breve_proposee' => 'ニュースは提出された',
+	'titre_breve_publiee' => 'ニュースは公開された',
+	'titre_breve_refusee' => 'ニュースは拒否された',
 	'titre_breves' => 'ニュース',
-	'titre_langue_breve' => 'ニュースの言語', # MODIF
+	'titre_langue_breve' => 'ニュースの言語',
+	'titre_nouvelle_breve' => '新規ニュース',
 	'titre_page_breves' => 'ニュース'
 );
